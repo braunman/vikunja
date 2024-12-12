310186dd844c821fc073236dd0520f4500ffba94
Andrey Braun <andrey.braun@easybrain.com>
2024-12-12 21:52:03 +0100

    update tests

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -13,22 +13,3 @@ test('Login with exist user @UI @LOGIN @POSITIVE', async ({ webApp }) => {
     await webApp.loginPage.loginUser(user)
     await expect(webApp.mainPage.loginUsername).toContainText(user.username)
 });
-
-
-[
-    { user: new User().build(), describe: "not exist user" },
-    { user: new User({ username: 'demo' }).build(), describe: "wrong password" },
-].forEach(({ user, describe }) => {
-    test(`Login with wrong credentials: ${describe} @UI @LOGIN @NEGATIVE`, async ({ webApp }) => {
-        await description("Try to login with wrong credentials")
-        await feature("login")
-        await webApp.loginPage.open()
-        await webApp.loginPage.loginUser(user)
-        await expect(webApp.loginPage.errorText).toContainText('Wrong username or password.')
-    });
-});
-
-
-
-
-
